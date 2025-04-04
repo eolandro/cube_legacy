1f719b01f71407511f687efad3edf48ba251e2a2
eolandro <eolandro@gmx.com>
2025-04-03 20:56:12 -0600

    First release

---
+++ enet/build/CMakeFiles/enet.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/callbacks.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/callbacks.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/host.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/host.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/list.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/list.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/packet.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/packet.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/peer.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/peer.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/protocol.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/protocol.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/unix.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/unix.c.o"
+  "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/src/win32.c" "/home/eolandro/Unstable/cpp/cube/cube_source/enet2/build/CMakeFiles/enet.dir/src/win32.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAS_FCNTL=1"
+  "HAS_GETADDRINFO=1"
+  "HAS_GETHOSTBYADDR_R=1"
+  "HAS_GETHOSTBYNAME_R=1"
+  "HAS_GETNAMEINFO=1"
+  "HAS_INET_NTOP=1"
+  "HAS_INET_PTON=1"
+  "HAS_MSGHDR_FLAGS=1"
+  "HAS_POLL=1"
+  "HAS_SOCKLEN_T=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ enet/build/CMakeFiles/enet.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enet.dir/src/callbacks.c.o"
+  "CMakeFiles/enet.dir/src/host.c.o"
+  "CMakeFiles/enet.dir/src/list.c.o"
+  "CMakeFiles/enet.dir/src/packet.c.o"
+  "CMakeFiles/enet.dir/src/peer.c.o"
+  "CMakeFiles/enet.dir/src/protocol.c.o"
+  "CMakeFiles/enet.dir/src/unix.c.o"
+  "CMakeFiles/enet.dir/src/win32.c.o"
+  "libenet.a"
+  "libenet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/enet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
